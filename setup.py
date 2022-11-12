f03ef877db05ccc8158e7f5c6c04d5c2cac9d062
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2022-11-12 01:36:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1a'
+VERSION = '0.1b'
 DESCRIPTION = 'A html binding to Tkinter UI'
 LONG_DESCRIPTION = 'Allows for html to design software with python tkinter.'
 
